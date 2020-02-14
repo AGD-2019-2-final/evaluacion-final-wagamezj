5dd0f525ab49b47349b9ee02d1e1d732f6f90e47
wilmer070 <wilmer070@gmail.com>
2020-02-14 14:32:21 -0500

    Update reducer.py

--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -2,3 +2,28 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    curkey = None
+    maximo = 0.0
+    minimo = 1000.0
+
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+            maximo = max(val,maximo)
+            minimo = min(val,minimo)
+
+        else:
+        
+            if curkey is not None:                
+
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo, minimo))
+
+            curkey = key
+            maximo = val
+            minimo = val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo, minimo))
