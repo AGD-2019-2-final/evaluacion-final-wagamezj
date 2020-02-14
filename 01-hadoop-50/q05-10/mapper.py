782a6d25650a91548252c422021d3a879153135b
wilmer070 <wilmer070@gmail.com>
2020-02-14 14:31:53 -0500

    Update mapper.py

--- 01-hadoop-50/q05-10/mapper.py
+++ 01-hadoop-50/q05-10/mapper.py
@@ -2,3 +2,8 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        mes = line.split('-')[1]
+        sys.stdout.write("{}\t1\n".format(mes))
