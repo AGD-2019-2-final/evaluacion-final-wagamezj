b47ff02998d9f9cd243e259855e7c430766c1e99
wilmer070 <wilmer070@gmail.com>
2020-02-14 14:31:33 -0500

    Update reducer.py

--- 01-hadoop-50/q02-10/reducer.py
+++ 01-hadoop-50/q02-10/reducer.py
@@ -2,3 +2,18 @@ import sys
 #
 #  >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+    currentk = None
+    t = 0
+    for line in sys.stdin:
+        k, v = line.split("\t")
+        v = int(v)
+        if k == currentk:
+            if v > t:
+                t = v
+        else:
+            if currentk is not None:
+                print(currentk + "\t" + str(t))
+            currentk = k
+            t = v
+    print(currentk + "\t" + str(t))
