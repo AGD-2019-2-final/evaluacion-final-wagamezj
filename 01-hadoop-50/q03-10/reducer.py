36a7d48245282e6c8b0d6eae51cda1012a4c6fc5
wilmer070 <wilmer070@gmail.com>
2020-02-14 14:31:40 -0500

    Update reducer.py

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -2,3 +2,10 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    for line in sys.stdin:
+        valor, key, valor2 = line.split("\t")
+        valor2=int(valor2)
+
+        sys.stdout.write("{},{}\n".format(key, valor2))
