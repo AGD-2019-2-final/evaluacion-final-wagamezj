8eb6de50ce1f60d77428cc069abd52e6b700b8f1
wilmer070 <wilmer070@gmail.com>
2020-02-14 14:41:56 -0500

    Update reducer.py

--- 01-hadoop-50/q10-10/reducer.py
+++ 01-hadoop-50/q10-10/reducer.py
@@ -2,3 +2,19 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+currentk = None
+nums = []
+
+for line in sys.stdin:
+    k, v = line.split("\t")
+    v = int(v)
+    if k == currentk:
+        nums.append(v)
+    else:
+        if currentk is not None:
+            nums.sort()
+            print(currentk + "\t" + str(nums).replace("[", "").replace("]", "").replace(" ", ""))
+        currentk = k
+        nums = [v]
+nums.sort()
+print(currentk + "\t" + str(nums).replace("[", "").replace("]", "").replace(" ", ""))
