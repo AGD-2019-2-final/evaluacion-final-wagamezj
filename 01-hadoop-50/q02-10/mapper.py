acad1a8967eed673d684a5542b589f3b9acb4fbb
wilmer070 <wilmer070@gmail.com>
2020-02-14 14:31:30 -0500

    Update mapper.py

--- 01-hadoop-50/q02-10/mapper.py
+++ 01-hadoop-50/q02-10/mapper.py
@@ -2,3 +2,13 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for line in sys.stdin:
+        purpose = ""
+        amount = ""
+        line = line.strip()
+        splits = line.split(",")
+        if splits[3] != 'purpose':
+            purpose = splits[3]
+            amount = splits[4]
+            print(purpose + "\t" + amount)
