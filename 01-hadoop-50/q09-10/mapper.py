eeccd9c4008c1a97613eb799e895fe49fe914d61
wilmer070 <wilmer070@gmail.com>
2020-02-14 14:40:30 -0500

    Update mapper.py

--- 01-hadoop-50/q09-10/mapper.py
+++ 01-hadoop-50/q09-10/mapper.py
@@ -2,3 +2,11 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for line in sys.stdin:
+      #line = line[:-1]
+      columna = line.split()[0]
+      columna1 = line.split()[1]
+      columna2 = line.split()[2]
+      columna2=columna2.zfill(3)
+      sys.stdout.write("{}\t{}\t{}\t{}\n".format(columna2, columna, columna1, int(columna2)))
