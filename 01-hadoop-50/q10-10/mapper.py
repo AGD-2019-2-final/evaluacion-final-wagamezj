e867ada7c667baeab6005c5bfdb251d1e1f0be4c
wilmer070 <wilmer070@gmail.com>
2020-02-14 14:40:37 -0500

    Update mapper.py

--- 01-hadoop-50/q10-10/mapper.py
+++ 01-hadoop-50/q10-10/mapper.py
@@ -2,4 +2,12 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-        
+for line in sys.stdin:
+    col1 = ""
+    col2 = ""
+    line = line.strip()
+    splits = line.split("\t")
+    col1 = splits[0]
+    col2 = splits[1]
+    for i in col2.split(","):
+        print(i + "\t" + col1)  
